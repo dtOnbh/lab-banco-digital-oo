2df0678330e3d5a729e585bb4cb21e3b7d90a2bc
dtOnbh <daltonsilva.felix@gmail.com>
2024-07-15 11:12:15 -0300

    Update Banco.java

--- src/Banco.java
+++ src/Banco.java
@@ -1,24 +1,40 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Banco {
 
-	private String nome;
-	private List<Conta> contas;
+    private String nome;
+    private List<Conta> contas;
 
-	public String getNome() {
-		return nome;
-	}
+    public Banco(String nome) {
+        this.nome = nome;
+        this.contas = new ArrayList<>();
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public List<Conta> getContas() {
-		return contas;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setContas(List<Conta> contas) {
-		this.contas = contas;
-	}
+    public List<Conta> getContas() {
+        return contas;
+    }
 
+    public void adicionarConta(Conta conta) {
+        this.contas.add(conta);
+    }
+
+    public void removerConta(Conta conta) {
+        this.contas.remove(conta);
+    }
+
+    public void listarContas() {
+        for (Conta conta : contas) {
+            System.out.println(conta.getNumero() + " - " + conta.getCliente().getNome());
+        }
+    }
 }
+
