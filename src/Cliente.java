bbf192d73dfdd42a001c96011cce8e46542b84f4
dtOnbh <daltonsilva.felix@gmail.com>
2024-07-15 11:11:55 -0300

    Update Cliente.java

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,14 +1,16 @@
-
 public class Cliente {
 
-	private String nome;
+    private String nome;
 
-	public String getNome() {
-		return nome;
-	}
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public String getNome() {
+        return nome;
+    }
 
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
